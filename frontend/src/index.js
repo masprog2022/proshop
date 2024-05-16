f225452a19914ff9322aba41415ef6d4f2240a83
masprog2022 <masprog2022@gmail.com>
2024-05-16 22:49:17 +0100

    feat: product details page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,12 +11,14 @@ import App from "./App";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
     </Route>
   )
 );
