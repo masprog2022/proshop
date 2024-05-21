c8c354fced273fe24c0ec9b787e273036207f464
masprog2022 <masprog2022@gmail.com>
2024-05-21 20:30:05 +0100

    feat: get products from database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
-import products from "./data/product.js";
+
+import productRouter from "./routes/productRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -13,13 +14,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRouter);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
