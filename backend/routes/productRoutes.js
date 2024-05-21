6406c053aadbf4fc419a57dec5f62d25cf19c557
masprog2022 <masprog2022@gmail.com>
2024-05-21 20:47:29 +0100

    feat: Custom Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,6 +8,7 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+    //throw new Error("Some error");
     res.json(products);
   })
 );
@@ -20,7 +21,8 @@ router.get(
     if (product) {
       return res.json(product);
     }
-    res.status(404).json({ message: "Product not found" });
+    res.status(404);
+    throw new Error("Rosource not found");
   })
 );
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 import productRouter from "./routes/productRoutes.js";
 dotenv.config();
@@ -14,7 +15,8 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use('/api/products', productRouter);
-
+app.use("/api/products", productRouter);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
