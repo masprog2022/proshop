8897bc51d1a52d11577143eb0cd63c232bb56dba
masprog2022 <masprog2022@gmail.com>
2024-05-23 00:02:30 +0100

    feat: create cartPage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import {
 import App from "./App";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
+import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import reportWebVitals from "./reportWebVitals";
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
       <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Route>
   )
 );
