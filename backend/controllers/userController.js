5d5c2f80791634a2b8f2e740ddc07e8d3ba55381
masprog2022 <masprog2022@gmail.com>
2024-05-23 18:48:33 +0100

    feat: logout user and clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,7 +43,11 @@ const registerUser = asyncHandler(async (req, res) => {});
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.send("Logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 };
 
 // @desc    Get user profile
