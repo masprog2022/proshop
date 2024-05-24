89e808dc8eafb1935ba3b02a0255a8868ddb034f
masprog2022 <masprog2022@gmail.com>
2024-05-24 14:25:05 +0100

    feat: create and get Orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,55 +1,111 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
+import { calcPrices } from "../utils/calcPrices.js";
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
- res.send('add order items')
+  const { orderItems, shippingAddress, paymentMethod } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    // NOTE: here we must assume that the prices from our client are incorrect.
+    // We must only trust the price of the item as it exists in
+    // our DB. This prevents a user paying whatever they want by hacking our client
+    // side code - https://gist.github.com/bushblade/725780e6043eaf59415fbaf6ca7376ff
+
+    // get the ordered items from our database
+    const itemsFromDB = await Product.find({
+      _id: { $in: orderItems.map((x) => x._id) },
+    });
+
+    // map over the order items and use the price from our items from database
+    const dbOrderItems = orderItems.map((itemFromClient) => {
+      const matchingItemFromDB = itemsFromDB.find(
+        (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
+      );
+      return {
+        ...itemFromClient,
+        product: itemFromClient._id,
+        price: matchingItemFromDB.price,
+        _id: undefined,
+      };
+    });
+
+    // calculate prices
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
+      calcPrices(dbOrderItems);
+
+    const order = new Order({
+      orderItems: dbOrderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc    Get logged in  user orders
 // @route   GET /api/orders/myOrders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send('get my orders')
- });
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
+});
 
- // @desc    Get order by ID
+// @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send('get order by id')
- });
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
 
- // @desc    Update order to paid
+// @desc    Update order to paid
 // @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('update order to paid')
- });
+  res.send("update order to paid");
+});
 
-  // @desc    Update order to delivered
+// @desc    Update order to delivered
 // @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send('update order to delivered')
- });
+  res.send("update order to delivered");
+});
 
-
-   // @desc    Get All orders
+// @desc    Get All orders
 // @route   GET /api/orders
 // @access  Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  res.send('get all orders')
- });
+  res.send("get all orders");
+});
 
- export {
+export {
   addOrderItems,
   getMyOrders,
   getOrderById,
-  updateOrderToPaid,
-  updateOrderToDelivered,
   getOrders,
-
- }
+  updateOrderToDelivered,
+  updateOrderToPaid,
+};
