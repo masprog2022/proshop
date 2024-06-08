e6fdb7d2620b8c0f970229ff7577eb32fb41f597
masprog2022 <masprog2022@@gmail.com>
2024-06-08 15:34:38 +0100

    feat: create reviews frontend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,7 +13,8 @@ import { admin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
-router.route("/:id/reviews").post(protect, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
+//router.get('/top', getTopProducts);
 
 export default router;
 
--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -45,6 +45,18 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
       providesTags: ['Product'],
     }),
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    getTopProducts: builder.query({
+      query: () => `${PRODUCTS_URL}/top`,
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
@@ -54,5 +66,6 @@ export const {
   useCreateProductMutation,
   useUpdateProductMutation,
   useUploadProductImageMutation,
-  useDeleteProductMutation
+  useDeleteProductMutation,
+  useCreateReviewMutation
 } = productsApiSlice;
