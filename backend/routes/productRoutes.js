464347e9e9c831269d9324a91b3fdf6709f42340
masprog2022 <masprog2022@@gmail.com>
2024-06-08 13:39:10 +0100

    feat: create reviews backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,7 +4,8 @@ import {
   getProductById,
   getProducts,
   updateProduct,
-  deleteProduct
+  deleteProduct,
+  createProductReview
 } from "../controllers/productController.js";
 
 import { admin, protect } from "../middleware/authMiddleware.js";
@@ -12,6 +13,7 @@ import { admin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProduct);
 
 export default router;
 
