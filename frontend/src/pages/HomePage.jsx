f6d876e8fe4f53f910942c0ef3598758da1b25a6
masprog2022 <masprog2022@@gmail.com>
2024-06-08 15:56:12 +0100

    feat: Paginate Products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 //import axios from "axios";
 //import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productApiSlice";
 
 const HomePage = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery( { pageNumber });
 
   return (
     <>
@@ -23,7 +25,7 @@ const HomePage = () => {
           {" "}
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
